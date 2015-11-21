aa839dc8d9d5d3e2d416f2482fbb2df4a58c9ef7
Ryan Tremblay <zaptrem@gmail.com>
2015-11-21 14:19:49 -0500

    Initial Commit

---
+++ Quality Of Area/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quality Of Area
+//
+//  Created by Ryan Tremblay on 11/21/15.
+//  Copyright © 2015 Ryan Tremblay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
