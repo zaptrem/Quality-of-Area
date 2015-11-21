1b966de8d61dc215510b4711b5482bb835856efc
Ryan Tremblay <zaptrem@gmail.com>
2015-11-21 14:32:07 -0500

    Added ScoreCalc.swift

---
+++ Quality Of Area/ScoreCalc.swift
@@ -0,0 +1,11 @@
+//
+//  ScoreCalc.swift
+//  Quality Of Area
+//
+//  Created by Ryan Tremblay on 11/21/15.
+//  Copyright © 2015 Ryan Tremblay. All rights reserved.
+//
+
+import Foundation
+
+// Calculate scores here. 
