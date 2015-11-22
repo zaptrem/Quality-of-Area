7d33b5082ec37db175696cf4eb565a77a6c12036
Ryan Tremblay <zaptrem@gmail.com>
2015-11-21 23:58:58 -0500

    fixed spelling, added parsing.

--- Quality Of Area/ViewController.swift
+++ Quality Of Area/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        let filePath = NSBundle.mainBundle().pathForResource("nyccrimedata2015", ofType: "json")
+        let data =  NSData(contentsOfFile: filePath!)
+        let json = JSON(data: data!)
+        var actualData = json["data"]
         // Do any additional setup after loading the view, typically from a nib.
         print("hello world.")
+        print()
     }
 
     override func didReceiveMemoryWarning() {
